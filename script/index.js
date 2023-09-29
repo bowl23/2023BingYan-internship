b8c300a6b7b99517dd51c5c5a9a11d1f757f7471
bowl23 <loop_bowl@qq.com>
2023-09-29 18:16:46 +0800

    第一个界面差不多给写完了

--- script/index.js
+++ script/index.js
@@ -1,6 +1,8 @@
 const pvpBtn = document.getElementById('pvp');
 const pveBtn = document.getElementById('pve');
 const back = document.getElementById('back');
+const people = document.getElementById('people')
+const bot = document.getElementById('bot')
 
 back.addEventListener('touchstart', () => {
     back.classList.add('touch-start');
@@ -9,4 +11,28 @@ back.addEventListener('touchstart', () => {
 back.addEventListener('touchend', () => {
     back.classList.add('untouch')
     back.classList.remove('touch-start')
+})
+pvpBtn.addEventListener('touchstart', () => {
+    people.classList.add('touch')
+    people.classList.remove('untouch')
+    pvpBtn.classList.add('touch-start');
+    pvpBtn.classList.remove('untouch')
+});
+pvpBtn.addEventListener('touchend', () => {
+    people.classList.add('untouch')
+    people.classList.remove('touch')
+    pvpBtn.classList.add('untouch')
+    pvpBtn.classList.remove('touch-start')
+})
+pveBtn.addEventListener('touchstart', () => {
+    bot.classList.add('touch')
+    bot.classList.remove('untouch')
+    pveBtn.classList.add('touch-start');
+    pveBtn.classList.remove('untouch')
+});
+pveBtn.addEventListener('touchend', () => {
+    bot.classList.add('untouch')
+    bot.classList.remove('touch')
+    pveBtn.classList.add('untouch')
+    pveBtn.classList.remove('touch-start')
 })
