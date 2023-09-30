f59fa812f33b9fd4c91a8e1619d5530ca32c3cd6
bowl23 <loop_bowl@qq.com>
2023-09-30 15:22:09 +0800

    第二个界面基本写完

---
+++ script/bot-choice.js
@@ -0,0 +1,21 @@
+const play = document.getElementById('play')
+const back = document.getElementById('back')
+const backText = document.querySelector('.bcak-text')
+
+play.addEventListener('touchstart', () => {
+    console.log('play');
+    play.classList.add('touch-start');
+    play.classList.remove('untouch');
+})
+play.addEventListener('touchend', () => {
+    play.classList.remove('touch-start');
+    play.classList.add('untouch');
+})
+back.addEventListener('touchstart', () => {
+    back.classList.add('touch-start');
+    back.classList.remove('untouch')
+})
+back.addEventListener('touchend', () => {
+    back.classList.remove('touch-start')
+    back.classList.add('untouch')
+})
